b545e0ad72908dd05e43e5575503c674c0806c0b
AlinaMarti <alina.marti@uzh.ch>
2020-06-23 20:26:07 +0200

    Add entry screen.

---
+++ E4tester/EntryController.swift
@@ -0,0 +1,20 @@
+//
+//  EntryController.swift
+//  E4tester
+//
+//  Created by Alina Marti on 23.06.20.
+//  Copyright © 2020 Felipe Castro. All rights reserved.
+//
+
+import UIKit
+
+class EntryController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
